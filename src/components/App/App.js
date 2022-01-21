ecbcc508930b3669767a79bf24cbbeafa4cb5aac
rubensysoecho <rubenmartinezTRABALLOS@gmail.com>
2022-01-21 13:15:22 +0100

    heading first contact

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-
+      <header className='header'>
+        <a href='' className='header-signup'>
+          Sign up
+        </a>
+        <a href='' className='header-signin'>
+          Sign in
+        </a>
+      </header>
     </div>
   );
 }
